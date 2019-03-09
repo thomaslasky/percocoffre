54352ac0ba14f0f0e7945a98ed0598d60df89014
ThomasTartas <thomastartas@crealis-ingenierie.com>
2019-03-09 15:08:50 +0100

    Update SiteActiveBlockManager.php

--- src/SiteActiveBlockManager.php
+++ src/SiteActiveBlockManager.php
@@ -16,10 +16,40 @@
 			$this->table = "site_active_block";
 		}
 		
-		public function AcBlockManager() {
+		public function AcBlockManager(SiteActiveBlock &$activeBlock) {
 			
 			if($this->isSetAcBlock()) {
 				
+				$sql = "INSERT INTO site_active_block (block_pres_1, block_pres_2, block_pres_3) VALUE (:block1 ,:block2 ,:block3)";
+				
+				$req = $this->db->prepare($sql);
+				
+				$req->bindValue('block1', $activeBlock->getBlockPres1(), \PDO::PARAM_BOOL);
+				$req->bindValue('block2', $activeBlock->getBlockPres2(), \PDO::PARAM_BOOL);
+				$req->bindValue('block3', $activeBlock->getBlockPres3(), \PDO::PARAM_BOOL);
+				
+				if($req->execute()) {
+					return true;
+				}
+				
+				return false;
+			} elseif($this->isSetAcBlock() === false) {
+				
+				$sql = "UPDATE {$this->table} SET block_pres_1 = :block1, block_pres_2 = :block2, block_pres_3 = :block3";
+				
+				$req = $this->db->prepare($sql);
+				
+				$req->bindValue('block1', $activeBlock->getBlockPres1(), \PDO::PARAM_BOOL);
+				$req->bindValue('block2', $activeBlock->getBlockPres2(), \PDO::PARAM_BOOL);
+				$req->bindValue('block3', $activeBlock->getBlockPres3(), \PDO::PARAM_BOOL);
+				
+				if($req->execute()) {
+					return true;
+				}
+				
+				return false;
+			} else {
+				return false;
 			}
 		}
 		
