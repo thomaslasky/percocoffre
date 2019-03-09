2764681c58345924c21fd6443ea7cc4f48d9f4c3
ThomasTartas <thomastartas@crealis-ingenierie.com>
2019-03-09 14:53:38 +0100

    Update AdminManager.php

--- src/AdminManager.php
+++ src/AdminManager.php
@@ -34,6 +34,48 @@
 		public function deleteAdmin($id) {
 			$sql = "DELETE FROM {$this->table} WHERE id = {$id}";
 			
+			$req = $this->db->prepare($sql);
 			
+			if($req->execute()) {
+				return true;
+			}
+			
+			return false;
+		}
+		
+		public function readAdmin($id) {
+			
+			$sql = "SELECT * FROM " . $this->table . " WHERE id = :id";
+			$req = $this->db->prepare($sql);
+			
+			$req->bindValue('id', $id, \PDO::PARAM_INT);
+			
+			$req->execute();
+			
+			$result = $req->fetch();
+			
+			return new Membres($result);
+		}
+		
+		public function readAllAdmin() {
+			$sql = "SELECT * FROM {$this->table}";
+			
+			$req = $this->db->prepare($sql);
+			$req->execute();
+			
+			$results = $req->fetchAll("FETCH_ASSOC");
+			
+			if(!empty($results)) {
+				
+				$tab = [];
+				
+				foreach($results as $result) {
+					$tab[] = new Admin($result);
+				}
+				
+				return $tab;
+			}
+			
+			return false;
 		}
 	}
